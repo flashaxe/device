14324fc545e7a9c6ff26265431bbacbacfcbadcc
flashaxe <flashaxe@gmail.com>
2013-02-19 16:13:23 +0800

    no more need

--- db.php
+++
@@ -1,8 +0,0 @@
-<?php
-$mysqlhost="localhost";
-$mysqluser="root";
-$mysqlpass="root";
-$db=mysql_connect($mysqlhost,$mysqluser,$mysqlpass);
-mysql_select_db("capinfoinfo",$db);
-mysql_query("SET NAMES 'utf8'");
-?>
